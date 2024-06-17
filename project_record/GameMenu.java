1fa34f5f3cdb7c315583c05853af133273128a06
KangYewon <ooooo712@naver.com>
2024-06-17 14:05:13 +0900

    gameMenu change (+again slove)

--- project_record/GameMenu.java
+++ project_record/GameMenu.java
@@ -4,14 +4,28 @@ import java.util.*;
 public class GameMenu implements Game {
 	private Scanner s = new Scanner(System.in);
 	private int totalScore =0;
+	private Map<String, Arithmetic> operations;
+	private AgainSolve againSolve;
+	private Records records;
+	
+	public GameMenu() {
+        operations = new HashMap<>();
+        operations.put("+", new Addition());
+        operations.put("-", new Subtraction());
+        operations.put("*", new Multiplication());
+        operations.put("/", new Division());
+
+        records = Records.createRecords();
+        againSolve = new AgainSolve(operations, records);
+    }
 	
 	@Override
 	public void start() {
-		Records records = Records.createRecords();
+		operations = new HashMap<>();
 		
 		while(true) {
 			System.out.println("-----------Game Start!-----------");
-			System.out.println("[1] Add\n[2] Subtract\n[3] Multiplication\n[4] Divide\n[5] View Records\n[0] Exit");
+			System.out.println("[1] Add\n[2] Subtract\n[3] Multiplication\n[4] Divide\n[5] View Records\n[6] Again solve\n[0] Exit");
 			System.out.println("Choose an operation: ");
 			int choice = s.nextInt();
 			
@@ -19,19 +33,23 @@ public class GameMenu implements Game {
 				records.displayRecords();
 				if (records.hasData()) {
 					System.out.println("Are you sure you want to clear all records? (yes/no)");
-			                String confirmation = s.nextLine();
-			                while (!confirmation.equalsIgnoreCase("yes") && !confirmation.equalsIgnoreCase("no")) {
-						System.out.println("Please enter 'yes' or 'no'.");
-			                    	confirmation = s.nextLine();
-			                }
-			                if (confirmation.equalsIgnoreCase("yes")) {
-			                    records.clearRecords();
-			                    System.out.println("All records have been cleared.");
-			                } else {
-			                    System.out.println("Clear records operation cancelled.");
-			                }
+	                String confirmation = s.nextLine();
+	                while (!confirmation.equalsIgnoreCase("yes") && !confirmation.equalsIgnoreCase("no")) {
+	                	System.out.println("Please enter 'yes' or 'no'.");
+	                    confirmation = s.nextLine();
+	                }
+	                if (confirmation.equalsIgnoreCase("yes")) {
+	                    records.clearRecords();
+	                    System.out.println("All records have been cleared.");
+	                } else if (confirmation.equalsIgnoreCase("no")) {
+	                    System.out.println("Clear records operation cancelled.");
+	                }
 				}
 				continue;
+			}
+			
+			if(choice == 6) {
+				 againSolve.solveIncorrectProblems();
 			}
 			
 			if(choice ==0) {
@@ -40,7 +58,6 @@ public class GameMenu implements Game {
 				System.out.println("GOOD BYE~");
 				break;
 			}
-			
 			Arithmetic arithmetic;
 			switch(choice) {
 			case 1:
@@ -55,6 +72,7 @@ public class GameMenu implements Game {
 			case 4:
 				arithmetic = new Division();
 				break;
+				
 			default:
 				System.out.println("Invalid choide!!");
 				continue;
