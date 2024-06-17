3b6e8d85dce5b6b4a4931bec039a37d5aafe642f
KangYewon <ooooo712@naver.com>
2024-06-17 14:03:38 +0900

    add again slove

---
+++ project_record/AgainSolve.java
@@ -0,0 +1,58 @@
+package TeamProject;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AgainSolve {
+
+    private Map<String, Arithmetic> operations;
+    private Records records;
+
+    public AgainSolve(Map<String, Arithmetic> operations, Records records) {
+        this.operations = operations;
+        this.records = records;
+    }
+
+    public void solveIncorrectProblems() {
+        Scanner scanner = new Scanner(System.in);
+        for (Iterator<String> iterator = records.getIncorrectAnswers().iterator(); iterator.hasNext();) {
+            String problem = iterator.next();
+            String[] parts = problem.split(" ");
+            int num1 = Integer.parseInt(parts[0]);
+            String operator = parts[1];
+            int num2 = Integer.parseInt(parts[2]);
+
+            Arithmetic operation = operations.get(operator);
+            if (operation == null) {
+                System.out.println("Unsupported operators: " + operator);
+                continue;
+            }
+
+            boolean validInput = false;
+            int userAnswer = 0;
+            
+            while (!validInput) {
+                try {
+                    System.out.print(num1 + " " + operator + " " + num2 + " = ");
+                    userAnswer = scanner.nextInt();
+                    validInput = true;
+                } catch (Exception e) {
+                    System.out.println("Invalid input.");
+                    scanner.next(); 
+                }
+            }
+            
+            boolean isCorrect = operation.checkAnswer(num1, num2, userAnswer);
+            
+            if (isCorrect) {
+                System.out.println("Succes!");
+                records.addScore(1);
+                iterator.remove(); 
+            } else {
+                System.out.println("Fail.");
+            }
+        }
+        records.saveRecords();
+    }
+}
